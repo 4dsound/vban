1445ad896b34506c232a61522a6a38c506ce81b6
Stijn van Beek <stijn.v.beek@gmail.com>
2025-05-17 14:56:13 +0200

    Set VBAN max packet size back to 1464 to stick to UDP protocol

--- src/vban/vban.h
+++ src/vban/vban.h
@@ -28,7 +28,7 @@ extern "C" {
 #define VBAN_HEADER_SIZE            (4 + 4 + 16 + 4) // 28
 #define VBAN_HEADER_FOURC           'NABV'
 #define VBAN_STREAM_NAME_SIZE       16
-#define VBAN_PROTOCOL_MAX_SIZE      65535
+#define VBAN_PROTOCOL_MAX_SIZE      1464
 #define VBAN_DATA_MAX_SIZE          (VBAN_PROTOCOL_MAX_SIZE - VBAN_HEADER_SIZE)
 #define VBAN_CHANNELS_MAX_NB        256
 #define VBAN_SAMPLES_MAX_NB         256
